8d3d4153d23aa923327fdbf0c4eec886900988af
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2015-06-25 23:05:01 +0530

    updated the problem description

--- Linkedlist_problem.java
+++ Linkedlist_problem.java
@@ -9,5 +9,8 @@
 public class Solution {
     public ListNode findLoopStartNode(ListNode list) {
         // TO-DO
+        // Input linkedlist has a loop, which if you do " while((list = list.next) != null ) ", 
+        // it will run as an infinite loop. The task is to find the node which the loop starts,
+        // in O(n) time complexity and O(1) memory complexity (with no additional memory)
     }
 }
