ab5cd1f0272297608f519006478d72de6ac22ad1
Vimukthi Weerasiri <vimukthiweerasiri@gmail.com>
2015-06-25 23:20:54 +0530

    adding the problem

---
+++ Linkedlist_problem.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode findLoopStartNode(ListNode list) {
+        // TO-DO
+        // Input linkedlist has a loop, which if you do " while((list = list.next) != null ) ",
+        // it will run as an infinite loop. The task is to find the node which the loop starts,
+        // in O(n) time complexity and O(1) memory complexity (with no additional memory)
+    }
+}
